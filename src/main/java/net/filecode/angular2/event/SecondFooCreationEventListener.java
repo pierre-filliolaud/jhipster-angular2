ae0be1ba9d77dd9d3f70e3a828615851aa2ef613
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2017-03-07 00:31:22 +0100

    add second event listener

--- src/main/java/net/filecode/angular2/event/SecondFooCreationEventListener.java
+++ src/main/java/net/filecode/angular2/event/SecondFooCreationEventListener.java
@@ -6,10 +6,10 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FooCreationEventListener implements ApplicationListener<FooCreationEvent> {
+public class SecondFooCreationEventListener implements ApplicationListener<FooCreationEvent> {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
    @Override
    public void onApplicationEvent(FooCreationEvent event) {
-	   log.info("Created foo instance: " + event.getSource().toString() + " entity:" + event.getEntity().toString());
+	   log.info("Created second foo instance: " + event.getSource().toString() + " entity:" + event.getEntity().toString());
    }
 }
